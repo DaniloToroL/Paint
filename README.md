df460ebc5a130848cc428517fc798fa47d94a88a
Danilo <danilo.toro@usach.cl>
2020-03-01 19:33:10 -0300

    add events and images

---
+++ README.md
@@ -0,0 +1,36 @@
+# Paint Web
+
+Project to practice Javascript with canvas
+
+## Learning
+
+### Javascript
+
+- Functions
+- Conditions
+- Listener
+
+### Css
+
+- Custom Cursor
+- Bootstrap
+
+Listeners
+
+## Done
+
+- Write
+- Pencil
+- Change Color
+- Eraser
+- Change Size
+
+## Todo
+
+- Get Color
+- Zoom 
+- Work with images
+- Figures
+- Fill
+
+
--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,15 @@
 
 // Variables
-let lapiz = false
+let isPencil = false
 let start = false
+let isWrite = false
+let text = ""
 let x_i
 let y_i
-let color = 'black'
-let size = 1
+let size = 5
+let isSize = false
+let color1 = '#000'
+let color2 = '#FFF'
 
 
 // HTML Objects
@@ -13,65 +17,120 @@ const canvas = document.getElementById('io')
 const lienzo = canvas.getContext('2d')
 const colorsRandom = document.getElementsByClassName('bg-color')
 
-// Listener
-canvas.addEventListener('mousedown', startDraw)
-canvas.addEventListener('mouseup',endDraw)
-canvas.addEventListener('mousemove', moving)
+document.getElementById('bg-color-1').style.backgroundColor = color1
+document.getElementById('bg-color-2').style.backgroundColor = color2
 
-for(color in colorsRandom){
-   if(colorsRandom[color].style != undefined){
+
+
+// Listener
+canvas.addEventListener('mousedown', startEvent)
+canvas.addEventListener('mouseup', endDraw)
+canvas.addEventListener('mousemove', moving)
+canvas.addEventListener('wheel', changeSize)
+canvas.addEventListener('click', function () {
+    if (isWrite) {
+        x_i = event.layerX
+        y_i = event.layerY
+        text = ""
+        start = true
+    }
+})
+
+document.addEventListener('keyup', pressKey)
+
+for (color in colorsRandom) {
+    colorsRandom[color].onclick = function(e){
+        color1 = this.style.backgroundColor
+        document.getElementById('bg-color-1').style.backgroundColor = color1
+    }
+    colorsRandom[color].oncontextmenu = function(e){
+        color2 = this.style.backgroundColor
+        document.getElementById('bg-color-2').style.backgroundColor = color2
+    }
+    if (colorsRandom[color].style != undefined) {
         colorsRandom[color].style.backgroundColor = getRandomColor()
-   }
+    }
 }
 
-
 // Functions
+
+function changeColor(){
+    const sizeImage = document.getElementById('size')
+    if (isSize){
+        sizeImage.classList.add("size-color")
+    }else{
+        sizeImage.classList.remove("size-color")
+    }
+}
 
 function getRandomColor() {
     var letters = '0123456789ABCDEF';
     var color = '#';
     for (var i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
+        color += letters[Math.floor(Math.random() * 16)];
     }
     return color;
-  }
-
-function changeColor(event){
-        color = event.target.value
 }
 
-function changeSize(event){
-    size = event.target.value
+
+
+function changeSize(event) {
+
+    if (isSize){
+        size += event.deltaY < 0 ? 5 : -5
+        size = size < 0 ? 1 : size
+    }
 }
 
-function startDraw(event) {
+function startEvent(event) {
     start = true
     x_i = event.layerX
-    y_i = event.layerY  
+    y_i = event.layerY
 }
 
-function endDraw(){
+function pressKey(event) {
+    text += event.key
+    if (start && isWrite) {
+        write()
+    }
+}
+
+function write() {
+    lienzo.fillStyle = color1;
+    lienzo.font = "bold Arial " + size;
+    lienzo.fillText(text, x_i, y_i);
+
+}
+
+function endDraw() {
     start = false
+    x_i = 0
+    y_i = 0
 }
 
-function moving(event){
-    if (lapiz && start){
+function moving(event) {
+    if (isPencil && start) {
         x_f = event.layerX
         y_f = event.layerY
-        draw(x_i, y_i, x_f, y_f )
+        drawRectangle(x_f, y_f, size, size)
         x_i = event.layerX
         y_i = event.layerY
     }
 }
 
-function draw(x_i, y_i, x_f, y_f){
+function draw(x_i, y_i, x_f, y_f) {
     lienzo.beginPath()
-    lienzo.strokeStyle = color
+    lienzo.strokeStyle = color1
     lienzo.lineWidth = size
-    lienzo.moveTo(x_i, y_i)  
+    lienzo.moveTo(x_i, y_i)
     lienzo.lineTo(x_f, y_f)
     lienzo.stroke()
     lienzo.closePath()
+}
+
+function drawRectangle(x, y, width, height){
+    lienzo.fillStyle = color1;
+    lienzo.fillRect(x, y, width, height);
 }
 
 
